10f948fa55ad2eab18f769eb21e0dfdedc417298
Abhi70815 <abhishekchurasia45621@gmail.com>
2025-05-31 17:18:22 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,41 @@ import LoginPopup from './components/LoginPopup/LoginPopup'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
-
   const [showLogin, setShowLogin] = useState(false);
-  //setting login show
 
   return (
     <>
-    {showLogin ? <LoginPopup setShowLogin={setShowLogin}/> : <></>}
-    <div className='app'>
-      <Navbar setShowLogin={setShowLogin}/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<PlaceOrder/>}/>
-       <Route path='/admin' element={<iframe src="https://admin-food.onrender.com/list" frameBorder="0" width="100%" height="100%"></iframe>}/>
-
-      </Routes>
-    </div>
-    <Footer/>
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
+      <div className='app min-h-screen flex flex-col'>
+        <Navbar setShowLogin={setShowLogin} />
+        <main className='flex-grow'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/order' element={<PlaceOrder />} />
+            <Route 
+              path='/admin' 
+              element={
+                <div className="w-full h-full min-h-[80vh]">
+                  <iframe 
+                    src="https://admin-food.onrender.com/list" 
+                    title="Admin Panel"
+                    frameBorder="0" 
+                    width="100%" 
+                    height="100%" 
+                    className="min-h-[80vh] w-full"
+                    style={{ minHeight: '80vh', border: 'none' }}
+                  ></iframe>
+                </div>
+              } 
+            />
+          </Routes>
+        </main>
+      </div>
+      <Footer />
     </>
   )
 }
 
 export default App
+
